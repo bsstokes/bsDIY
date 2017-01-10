119018e894c65804c9889829372c7a02e718ae88
Brian Stokes <bsstokes@gmail.com>
2017-01-10 16:44:23 -0600

    Update release-build to reflect recent refactoring.

--- app/src/release/java/com/bsstokes/bsdiy/application/initializers/StethoInitializer.java
+++ app/src/release/java/com/bsstokes/bsdiy/application/initializers/StethoInitializer.java
@@ -1,6 +1,6 @@
 package com.bsstokes.bsdiy.application.initializers;
 
-import com.bsstokes.bsdiy.BsDiyApplication;
+import com.bsstokes.bsdiy.application.BsDiyApplication;
 
 public class StethoInitializer {
     @SuppressWarnings("UnusedParameters")
