1f7f2fc02243e0aad2a174cc4a933632ab913981
Brian Stokes <bsstokes@gmail.com>
2017-01-10 16:20:22 -0600

    Reorganize commented out fields of forthcoming Skill data.

--- app/src/main/java/com/bsstokes/bsdiy/api/DiyApi.java
+++ app/src/main/java/com/bsstokes/bsdiy/api/DiyApi.java
@@ -74,38 +74,32 @@ public interface DiyApi {
 
     class Skill {
         long id;
-        //                "sku":314889437,
-//                "stamp":"2014-05-06T22:53:11.000Z",
+        // "sku":314889437,
+        // "stamp":"2014-05-06T22:53:11.000Z",
         boolean active;
-        //                "activeAt":null,
+        // "activeAt":null,
         String url = "";
         String title = "";
-//                "description":
-//            "Actors bring characters to life. We conjure up emotion and laughter with our words and stories. The world is our stage, and we are players in it.",
-//                    "position":[
-//            0,
-//                    0
-//            ],
-//            "icons":{
-//            "small":"//d1973c4qjhao9m.cloudfront.net/patches/actor_icon_small.png",
-//                    "medium":"//d1973c4qjhao9m.cloudfront.net/patches/actor_icon_medium.png"
-//        },
-//            "images":{
-//            "small":"//d1973c4qjhao9m.cloudfront.net/patches/actor_icon_small.png",
-//                    "medium":"//d1973c4qjhao9m.cloudfront.net/patches/actor_medium.png",
-//                    "large":"//d1973c4qjhao9m.cloudfront.net/patches/actor_large.png"
-//        },
-//            "grammar":{
-//            "singular":"actor",
-//                    "plural":"Actor",
-//                    "subject":"Actor",
-//                    "article":"a"
-//        },
-//            "pole":"art",
-//                "color":"#005ce0",
-//                "notes":""
-//        }
-
+        // "description": "Actors bring characters to life. We conjure up emotion and laughter with our words and stories. The world is our stage, and we are players in it.",
+        // "position": [0, 0],
+        // "icons": {
+        //   "small":"//d1973c4qjhao9m.cloudfront.net/patches/actor_icon_small.png",
+        //   "medium":"//d1973c4qjhao9m.cloudfront.net/patches/actor_icon_medium.png"
+        // },
+        // "images": {
+        //   "small":"//d1973c4qjhao9m.cloudfront.net/patches/actor_icon_small.png",
+        //   "medium":"//d1973c4qjhao9m.cloudfront.net/patches/actor_medium.png",
+        //   "large":"//d1973c4qjhao9m.cloudfront.net/patches/actor_large.png"
+        // },
+        // "grammar": {
+        //   "singular":"actor",
+        //   "plural":"Actor",
+        //   "subject":"Actor",
+        //   "article":"a"
+        // },
+        // "pole": "art",
+        // "color": "#005ce0",
+        // "notes": ""
 
         @Override
         public String toString() {
