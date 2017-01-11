7ab1bbbfb6eb9413c240eb7f70ac92aeddb58b93
Brian Stokes <bsstokes@gmail.com>
2017-01-10 23:00:54 -0600

    Remove unnecessary log.

--- app/src/main/java/com/bsstokes/bsdiy/SkillsAdapter.java
+++ app/src/main/java/com/bsstokes/bsdiy/SkillsAdapter.java
@@ -5,7 +5,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -46,7 +45,6 @@ class SkillsAdapter extends RecyclerView.Adapter<SkillsAdapter.ViewHolder> {
         holder.skillTitleTextView.setText(skill.title);
 
         final String imageUrl = chooseImage(skill);
-        Log.d("SkillsAdapter", "onBindViewHolder: [" + position + "] imageUrl=" + imageUrl);
         if (!TextUtils.isEmpty(imageUrl)) {
             picasso.load(imageUrl).into(holder.skillPatchImageView);
         }
