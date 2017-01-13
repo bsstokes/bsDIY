2168bd3966232068d53636e6118d2f6b285a1482
Brian Stokes <bsstokes@gmail.com>
2017-01-13 10:03:12 -0600

    Show preliminary Challenge info on Skill screen.

--- app/src/main/java/com/bsstokes/bsdiy/SkillActivity.java
+++ app/src/main/java/com/bsstokes/bsdiy/SkillActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,12 +16,13 @@ import com.bsstokes.bsdiy.db.BsDiyDatabase;
 import com.bsstokes.bsdiy.sync.ApiSyncService;
 import com.squareup.picasso.Picasso;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import dagger.internal.Preconditions;
-import rx.Observable;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
@@ -39,12 +42,15 @@ public class SkillActivity extends AppCompatActivity {
 
     @BindView(R.id.patch_image_view) ImageView patchImageView;
     @BindView(R.id.description_text_view) TextView descriptionTextView;
+    @BindView(R.id.challenges_layout) ViewGroup challengesViewGroup;
 
     @Inject BsDiyDatabase database;
     @Inject Picasso picasso;
 
     private long skillId = 0;
     private final CompositeSubscription subscriptions = new CompositeSubscription();
+
+    private static final String TAG = "SkillActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,19 +73,27 @@ public class SkillActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        final Observable<DiyApi.Skill> getSkill = database.getSkill(skillId)
+        final Subscription getSkill = database.getSkill(skillId)
                 .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
-
-        final Subscription subscription = getSkill
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Action1<DiyApi.Skill>() {
                     @Override
                     public void call(DiyApi.Skill skill) {
                         onLoadSkill(skill);
                     }
                 });
+        subscriptions.add(getSkill);
 
-        subscriptions.add(subscription);
+        final Subscription getChallenges = database.getChallenges(skillId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<List<DiyApi.Challenge>>() {
+                    @Override
+                    public void call(List<DiyApi.Challenge> challenges) {
+                        onLoadChallenges(challenges);
+                    }
+                });
+        subscriptions.add(getChallenges);
     }
 
     @Override
@@ -89,11 +103,31 @@ public class SkillActivity extends AppCompatActivity {
     }
 
     private void onLoadSkill(DiyApi.Skill skill) {
+        if (null != skill && null != skill.url) {
+            // TODO: Not the best place to put this because it might be firing
+            // too often.
+            ApiSyncService.syncChallenges(this, skill.url);
+        }
+
         setTitle(skill.title);
 
         final String imageUrl = DiyApi.Helper.normalizeUrl(skill.images.large);
         picasso.load(imageUrl).into(patchImageView);
 
         descriptionTextView.setText(skill.description);
+    }
+
+    private void onLoadChallenges(List<DiyApi.Challenge> challenges) {
+        challengesViewGroup.removeAllViews();
+        for (final DiyApi.Challenge challenge : challenges) {
+            onLoadChallenge(challenge);
+        }
+    }
+
+    private void onLoadChallenge(DiyApi.Challenge challenge) {
+        final View view = getLayoutInflater().inflate(R.layout.challenge_list_item_layout, challengesViewGroup, false);
+        final TextView titleTextView = ButterKnife.findById(view, R.id.title_text_view);
+        titleTextView.setText(challenge.title);
+        challengesViewGroup.addView(view);
     }
 }
