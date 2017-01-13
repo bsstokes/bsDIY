fc7608bb5b1c4117ef28bc384e1d30fe73868c6b
Brian Stokes <bsstokes@gmail.com>
2017-01-12 21:10:41 -0600

    Reorganize SkillMapper to group other mappers together.

--- app/src/main/java/com/bsstokes/bsdiy/db/sqlite/mappers/SkillMapper.java
+++ app/src/main/java/com/bsstokes/bsdiy/db/sqlite/mappers/SkillMapper.java
@@ -7,21 +7,23 @@ import com.bsstokes.bsdiy.db.sqlite.Db;
 
 import rx.functions.Func1;
 
-public class SkillMapper implements Func1<Cursor, DiyApi.Skill> {
-    @Override
-    public DiyApi.Skill call(Cursor cursor) {
-        final DiyApi.Skill skill = new DiyApi.Skill();
-        skill.id = Db.getLong(cursor, "_id");
-        skill.active = Db.getBoolean(cursor, "active");
-        skill.url = Db.getString(cursor, "url");
-        skill.title = Db.getString(cursor, "title");
-        skill.description = Db.getString(cursor, "description");
-        skill.images = new DiyApi.Skill.Images(
-                Db.getString(cursor, "image_small"),
-                Db.getString(cursor, "image_medium"),
-                Db.getString(cursor, "image_large")
-        );
-        skill.color = Db.getString(cursor, "color");
-        return skill;
+public interface SkillMapper {
+    class CursorToSkill implements Func1<Cursor, DiyApi.Skill> {
+        @Override
+        public DiyApi.Skill call(Cursor cursor) {
+            final DiyApi.Skill skill = new DiyApi.Skill();
+            skill.id = Db.getLong(cursor, "_id");
+            skill.active = Db.getBoolean(cursor, "active");
+            skill.url = Db.getString(cursor, "url");
+            skill.title = Db.getString(cursor, "title");
+            skill.description = Db.getString(cursor, "description");
+            skill.images = new DiyApi.Skill.Images(
+                    Db.getString(cursor, "image_small"),
+                    Db.getString(cursor, "image_medium"),
+                    Db.getString(cursor, "image_large")
+            );
+            skill.color = Db.getString(cursor, "color");
+            return skill;
+        }
     }
 }
