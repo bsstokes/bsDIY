799781d3b72b115a1ab56dd0d6293c12d3f2b60b
Brian Stokes <bsstokes@gmail.com>
2017-01-18 14:18:32 -0600

    Install LeakCanary.

--- app/src/main/java/com/bsstokes/bsdiy/application/BsDiyApplication.java
+++ app/src/main/java/com/bsstokes/bsdiy/application/BsDiyApplication.java
@@ -9,6 +9,7 @@ import com.bsstokes.bsdiy.application.di.AppComponent;
 import com.bsstokes.bsdiy.application.di.ComponentFactory;
 import com.bsstokes.bsdiy.application.initializers.StethoInitializer;
 import com.crashlytics.android.Crashlytics;
+import com.squareup.leakcanary.LeakCanary;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -29,6 +30,11 @@ public class BsDiyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        LeakCanary.install(this);
 
         Fabric.with(this, new Crashlytics());
 
